50b59e9f487d8a41598be154a239a7c2e49754c1
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2023-11-02 15:47:17 -0800

    feat: parameter input values for model training

--- ML_GUI/module/models.py
+++ ML_GUI/module/models.py
@@ -4,14 +4,62 @@ from sklearn.linear_model import SGDRegressor
 import matplotlib.pyplot as plt
 import module.output as output
 
-class ModelSelection():
-    def simple_linear_regression(self, x_train_scaled, y_train_scaled, x_test_scaled, y_test_scaled, alpha, iterations):
+
+class ModelSelection:
+    def simple_linear_regression(
+        self,
+        x_train_scaled,
+        y_train_scaled,
+        x_test_scaled,
+        y_test_scaled,
+        loss,
+        penalty,
+        alpha,
+        l1_ratio,
+        fit_intercept,
+        iterations,
+        tol,
+        shuffle,
+        verbose,
+        epsilon,
+        random_state,
+        learning_rate,
+        eta0,
+        power_t,
+        early_stopping,
+        validation_fraction,
+        n_iter_no_change,
+        warm_start,
+        average,
+    ):
         """Train simple linear regression model, plot predictions, and the original data."""
 
         # Model Initialization
-        regressor = SGDRegressor(alpha=alpha, max_iter=iterations)
-        regressor.fit(x_train_scaled, y_train_scaled)
-        y_pred = regressor.predict(self.x_test_scaled)
+        regressor = SGDRegressor(
+            loss=loss,
+            penalty=penalty,
+            alpha=alpha,
+            l1_ratio=l1_ratio,
+            fit_intercept=fit_intercept,
+            max_iter=iterations,
+            tol=tol,
+            shuffle=shuffle,
+            verbose=verbose,
+            epsilon=epsilon,
+            random_state=random_state,
+            learning_rate=learning_rate,
+            eta0=eta0,
+            power_t=power_t,
+            early_stopping=early_stopping,
+            validation_fraction=validation_fraction,
+            n_iter_no_change=n_iter_no_change,
+            warm_start=warm_start,
+            average=average,
+        )
+        regressor.fit(
+            x_train_scaled, y_train_scaled
+        )  # Pass y_train_scaled to fit method
+        y_pred = regressor.predict(x_test_scaled)
         self.y_pred = y_pred
         # print("y_pred: ", self.y_pred.shape)
         # print("x_test: ", self.x_test_scaled.shape)
@@ -19,12 +67,12 @@ class ModelSelection():
         # Plotting Results
         plt.clf()
         fig, axes = plt.subplots(figsize=(8, 6))
-        plt.plot(self.y_test_scaled, self.y_pred,  'go', label='True data', alpha=0.5)
-        plt.plot(self.x_test_scaled, self.y_pred, '--', label='Predictions', alpha=0.5)
+        plt.plot(y_test_scaled, y_pred, "go", label="True data", alpha=0.5)
+        plt.plot(x_test_scaled, y_pred, "--", label="Predictions", alpha=0.5)
 
-        plt.xlabel('Independent Variable')
-        plt.ylabel('Dependent Variable')
-        plt.title('Simple Linear Regression')
+        plt.xlabel("Independent Variable")
+        plt.ylabel("Dependent Variable")
+        plt.title("Simple Linear Regression")
         plt.legend()
         plt.show()
 
